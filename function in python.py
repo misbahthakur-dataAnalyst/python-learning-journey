44ee469578a9088712839ffb4135c5a342444033
misbahthakur-dataAnalyst <misbahthakur95@gmail.com>
2026-09-12 18:16:28 +0530

    Add welcome_message and inspire functions
    Added two functions: welcome_message() to print a welcome message three times and inspire() to print a motivational quote.

---
+++ function in python.py
@@ -0,0 +1,19 @@
+Write a function named welcome_message() that prints “Welcome to 
+#Python Programming!” three times.
+
+def welcome_message():
+    print("welcome to python programming!")
+    print("line 2")
+
+
+welcome_message() #function is being called here
+welcome_message()
+welcome_message()
+
+#Define a function inspire() that prints a motivational quote with your 
+#name. 
+
+def inspire():
+    print("you are the master of your destiny : misu")
+
+inspire()
