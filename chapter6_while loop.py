2fe1f66c00981df616c4a23479fc68bbeb76872a
misbahthakur-dataAnalyst <misbahthakur95@gmail.com>
2026-09-12 11:37:08 +0530

    Add while loop to print even numbers from 1 to 50
    Implement a program to print even numbers from 1 to 50 using a while loop.

---
+++ chapter6_while loop.py
@@ -0,0 +1,11 @@
+ write a program to print all even numbers between 1 to 50 using a while loop
+
+#e even number= 2,4,6,8
+
+num= 1
+
+while(num<=50):
+    if(num%2 ==0):
+        print(num)
+
+    num= num+1    
