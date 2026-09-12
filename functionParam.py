9e811dc9e44b10b7c6a808d2c60f49cabeb5ef94
misbahthakur-dataAnalyst <misbahthakur95@gmail.com>
2026-09-12 19:10:42 +0530

    Implement average and show_age functions
    Added functions to calculate average and show age.

---
+++ functionParam.py
@@ -0,0 +1,21 @@
+function definition with parameter
+def average(a,b):
+    averageValue= (a+b)/2
+    print(averageValue)
+
+
+# function calling with argument
+average(5,10)
+average(7,10)
+average(80,98)
+average(2,4)
+
+# Write a function show_age(name, age) that prints: "Saumya Singh is 21 
+#years old."
+
+def show_age(name="misbah thakur", age=23):
+    print(f"{name} is {age} years old")
+
+show_age("misbah thakur", 23)
+show_age()
+show_age("madeeha", 19)
